7ba7648186b446178fa42b6db2578021206e7adb
Oleksii Khrustalov <oleksiwe22@gmail.com>
2024-05-30 17:35:18 +0300

    added calculator expressions

---
+++ main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'calculations'
+
+puts six(times(four))        # Output: 24
+puts four(plus(zero))        # Output: 4
+puts eight(minus(two))       # Output: 6
+puts six(divided_by(four))   # Output: 1
